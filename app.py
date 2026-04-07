ee5d68f46688f081777d87b25eb19a1621564263
Oyeyinka Oyeniyi <yinkaoyeniyi10@gmail.com>
2026-04-07 18:19:38 -0500

    Added a working delete and update option

--- app.py
+++ app.py
@@ -26,6 +26,27 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task!"
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    tasks = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        tasks.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue updating your task."
+    else:
+        return render_template('update.html', tasks=tasks)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
